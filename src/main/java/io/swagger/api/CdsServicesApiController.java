ca3b16fbda15cf6abe5a12d1fc7daac700ad94d5
erika <adlere3001@gmail.com>
2023-04-16 01:48:36 +0200

    fixed post example

--- src/main/java/io/swagger/api/CdsServicesApiController.java
+++ src/main/java/io/swagger/api/CdsServicesApiController.java
@@ -41,8 +41,7 @@ public class CdsServicesApiController implements CdsServicesApi {
     @RequestMapping(value = "/cds-services/template", produces = {"application/json"}, consumes = {"application/json"}, method = RequestMethod.POST) // @RequestHeader(name = "Authorization") String token,
     public ResponseEntity<CDSResponse> cdsTestService(@ApiParam(value = "Body of CDS service request", required = true) @Valid @RequestBody CDSRequest request, @RequestHeader Map<String, String> headers) {
         logger.info("CDS Hook: template is triggered");
-        Object x = request.getPrefetch();
-        System.out.println(x);
+        System.out.println(request.toString());
         //ResponseEntity<CDSResponse> cdsResponseResponseEntity = new ResearchStudyQueryService(token).suggestResearchStudies(request, token);
         return new ResponseEntity<>(HttpStatus.OK);
     }
